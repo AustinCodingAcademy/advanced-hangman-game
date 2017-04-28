f990ea29ecbd11b59be3698351e1bff11b0a33a6
Dustin Wells <dustinwells@Dustins-MacBook-Pro.local>
2017-04-27 20:06:26 -0500

    Finished hangman game with win/loss conditions

--- src/App.js
+++ src/App.js
@@ -12,52 +12,74 @@ class App extends Component {
 			strikes: 0,
 			guess: "",
 			correctGuesses: [],
-			guessArr: this.state.wordToGuess.map(function (){
-				return " _ "
-			})
+			guessArr: ""
+
 	}
+	this.state.guessArr = this.state.wordToGuess.map(function(){
+			return " _ "
+	})
 }
+
+	mapArray(arr) {
+		var temp = [];
+		temp = arr.map(function () {
+			return " _ "
+		})
+		return temp;
+	}
+
   render() {
 		let _wordArr = this.state.wordToGuess;
 		let _guessArr = this.state.guessArr;
-    let className = `strike-${this.state.strikes}`;
+		console.log(_wordArr);
+		let currentStrike = this.state.strikes;
+		let spriteStrike = '';
+		let winner = true;
+		for(var i = 0; i < _guessArr.length; i++){
+			if(_guessArr[i] === ' _ ')
+				winner = false;
+		}
+		if(winner){
+			spriteStrike = "gamewon"
+		}else if (currentStrike < 6)
+			spriteStrike = `strike-${this.state.strikes}`;
+		else {
+			spriteStrike = "gameover"
+		}
     return (
 			<div>
 				<div  className="hangman-sprites">
-					<div className={`${className} current`} />
+					<div className={`${spriteStrike} current`} />
 				</div>
 				<div id="inputs">
 					<div>{this.state.guessArr}</div>
 					<input value={this.state.guess} onChange={
 		            (e) => {
-              if(e.target.value.length == 0) {
+              if(e.target.value.length <= 1 && currentStrike < 6) {
 		              this.setState({guess: e.target.value})
 									}
 		             }
 		           }  />
 					<button onClick={
 						(e) => {
-							let currentStrike = this.state.strikes;
+
 							let guessCounter = 0;
 							let tempGuess = this.state.correctGuesses;
 							for(var i = 0; i < _wordArr.length; i++) {
 								if (this.state.guess === _wordArr[i]) {
 									guessCounter++;
 									_guessArr[i] = this.state.guess;
-									this.setState({guessArr: _guessArr[i]})
+									this.setState({guessArr: _guessArr})
 									tempGuess.push(this.state.guess);
 								}
 							}
-							//I had to do this if statement twice for some reason, when I tried
-							// to place the current strike ++ inside the if statement below
-							// I got an error... was weird.
-							if(guessCounter===0)
-								currentStrike++;
+
  							if(guessCounter===0){
+								currentStrike++;
 								this.setState({strikes: currentStrike})
 							}
-								else {this.setState({correctGuesses: tempGuess})}
 
+								else {this.setState({correctGuesses: tempGuess})}
 						}
 					}>Guess</button>
 				</div>
