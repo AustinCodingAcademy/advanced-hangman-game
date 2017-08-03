47622635ffadeeabb2462edd36580a481a199d75
Greg Devany <greg.devany@yahoo.com>
2017-08-03 15:02:48 -0500

    Started. Done with ReadMe lines 1-8

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import state from "./state";
+// import randomWords from 'random-words';
+
+var randomWords = require('random-words');
+
 
 class App extends Component {
 	constructor(){
 		super();
 		this.state = {
-			wordToGuess:"",
+			wordToGuess:randomWords(),
 			strikes:0,
 			guess:"",
 			correctGuesses:[]
 		};
 	}
+
+fillGuesses = () => {
+  for(var i = 1; i <= this.state.wordToGuess.length; i++) {
+    this.state.correctGuesses.push('_');
+  }
+}
+
   render() {
+    // const newWord = randomWords();
+    console.log(this.state.wordToGuess);
+    this.fillGuesses();
+    console.log(this.state.correctGuesses);
 		let className = `strike-${this.state.strikes}`;
 		let spans = [<span>_</span>];
     return (
