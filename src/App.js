15c6b821d12f86285cfa31c1f8f5f38fbf7f22ff
Greg Devany <greg.devany@yahoo.com>
2017-08-04 00:26:37 -0500

    Started. progress

--- src/App.js
+++ src/App.js
@@ -19,36 +19,51 @@ class App extends Component {
 	}
 
 
-fillGuesses = () => {
-  if(this.state.correctGuesses.length < 1) {
-    for(var i = 1; i <= this.state.wordToGuess.length; i++) {
-      this.state.correctGuesses.push('_');
+  fillGuesses = () => {
+    if(this.state.correctGuesses.length < 1) {
+      for(var i = 1; i <= this.state.wordToGuess.length; i++) {
+        this.state.correctGuesses.push('_');
+      }
     }
   }
-}
 
-guessLetter = (e) => {
-  e.preventDefault();
-  if(e.target.value.length < 2) {
-    this.setState({
-      guess: e.target.value
-    });
+  guessLetter = (e) => {
+    e.preventDefault();
+    if(e.target.value.length < 2) {
+      this.setState({
+        guess: e.target.value
+      });
+    }
   }
-}
 
-buttonClick = () => {
-  const word = this.state.wordToGuess;
-  var temp = this.word.filter((letterPos) => {
-    return letterPos.indexOf(this.state.wordToGuess);
-  });
-  console.log(temp);
-}
+  buttonClick = () => {
+    const word = this.state.wordToGuess;
+    console.log(this.state.wordToGuess);
+
+    for(var i = 0; i < word.length; i++) {
+      if(word[i] === this.state.guess) {
+        this.showHit(i)
+      }
+    }
+  };
+
+  showHit = (i) => {
+    // console.log(i);
+    var tempCG = this.state.correctGuesses.slice(0);
+
+    tempCG.splice(i, 1, 'x');
+    this.setState({
+      correctGuesses: tempCG
+    })
+
+console.log(this.state.correctGuesses);
+   };
 
   render() {
     // const newWord = randomWords();
     this.fillGuesses();
-    console.log(this.state.wordToGuess);
-    console.log(this.state.guess);
+    // console.log(this.state.wordToGuess);
+    // console.log(`guess: this.state.guess`);
     console.log(this.state.correctGuesses);
 		let className = `strike-${this.state.strikes}`;
 		let spans = [<span>_</span>];
