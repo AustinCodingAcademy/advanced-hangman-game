68501e36346075824f2a8bb21cc305e5f6edb13e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-08-11 23:11:33 -0500

    generate random word, fill array with '_' and add event listener to the input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import state from "./state";
+import randomWords from "random-words";
 
 class App extends Component {
 	constructor(){
@@ -12,8 +13,24 @@ class App extends Component {
 			guess:"",
 			correctGuesses:[]
 		};
+		this.handleInput = this.handleInput.bind(this);
 	}
+
+	handleInput(e) {
+		if(e.target.value <= 1)
+		this.setState({
+			guess: e.target.value,
+		});
+	}
+
   render() {
+		console.log(this.state.guess);
+		this.state.wordToGuess = randomWords();
+		console.log(this.state.wordToGuess);
+
+		this.state.correctGuesses = Array(this.state.wordToGuess.length).fill("_")
+		console.log(this.state.correctGuesses);
+
 		let className = `strike-${this.state.strikes}`;
 		let spans = [<span>_</span>];
     return (
@@ -23,7 +40,7 @@ class App extends Component {
 				</div>
 				<div id="inputs">
 					<div>{spans}</div>
-					<input />
+					<input onChange={this.handleInput} />
 					<button>Guess</button>
 				</div>
 			</div>
