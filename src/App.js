aabdbff1237e7581d9fcacbaba90b450df825ca1
Craig Copeland <copelandhouse2@yahoo.com>
2017-11-13 16:00:37 -0600

    Initial push.  Completed assignment.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import state from "./state";
+import randomWords from 'random-words';
 
 class App extends Component {
 	constructor(){
@@ -13,9 +14,58 @@ class App extends Component {
 			correctGuesses:[]
 		};
 	}
+
+	componentDidMount() {
+		const newWord = randomWords();
+		let initArr = [];
+		initArr.length = newWord.length;
+		initArr.fill("_");
+		// console.log('componentDidMount',newWord, initArr);
+		this.setState({
+			wordToGuess:newWord,
+			correctGuesses:initArr
+		})
+	}
+
+	handleClick = ()=> {
+		// console.log('inHandleClick', this.state);
+		const { guess, wordToGuess, correctGuesses, strikes } = this.state;
+		if (strikes == 6) {
+			this.setState({ guess:"" });
+		} else if (wordToGuess.indexOf(guess) !== -1) {
+			let updatedGuesses = correctGuesses.map((pos, idx)=>{
+				if (wordToGuess[idx] === guess) {
+					// console.log('handle Click match', pos, idx, wordToGuess[idx], guess);
+					return guess;
+				} else {
+					// console.log('handle Click no match', pos, idx, wordToGuess[idx], guess);
+					return pos;
+				};
+			});
+			this.setState({
+				correctGuesses:updatedGuesses,
+				guess:""
+		 	});
+
+		} else {
+				this.setState({
+					strikes:strikes+1,
+					guess:""
+				});
+			}
+	}
+
   render() {
-		let className = `strike-${this.state.strikes}`;
-		let spans = [<span>_</span>];
+		// console.log('In render',this.state.wordToGuess, this.state.correctGuesses);
+		let className = ''
+		if (this.state.correctGuesses.indexOf("_") === -1) {
+			className = 'gamewon';
+		} else if (this.state.strikes == 6) {
+			className = 'gameover';
+		} else {
+			className = `strike-${this.state.strikes}`;
+		}
+		let spans = this.state.correctGuesses.map((guess, idx)=><span key={idx}>{guess}</span>);
     return (
 			<div>
 				<div  className="hangman-sprites">
@@ -23,8 +73,11 @@ class App extends Component {
 				</div>
 				<div id="inputs">
 					<div>{spans}</div>
-					<input />
-					<button>Guess</button>
+					<input
+						value={this.state.guess}
+						onChange = {(e)=> this.setState({ guess: e.target.value.toLowerCase().slice(0,1) })}
+					/>
+					<button onClick={this.handleClick}>Guess</button>
 				</div>
 			</div>
     );
